11dff72978c7e46a96fce937e0ad17ca5ecde9bc
Jpendy <Jpendy256@gmail.com>
2020-06-02 16:37:24 -0700

    got it all working

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const minimist = require('minimist')
 const { Notes } = require('./lib/Notes')
 const Input = require('./lib/Input')
 
 
-const input = new Input(minimist(process.argv));
-
-const notes = Notes.execute(input);
-
-console.log(notes)
+const input = new Input(process.argv);
+Input.valid && Notes.execute(input);
