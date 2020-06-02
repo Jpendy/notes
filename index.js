519890db12432fba480caafd837474e11aa26bc2
Jpendy <Jpendy256@gmail.com>
2020-06-02 10:21:24 -0700

    commit

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ const { execute } = require('./lib/notes')
 
 
 console.log(parse(process.argv))
+
+const action = parse(process.argv);
+const isValid = valid(action);
+
+if(isValid) execute(action);
+else console.log('bad command')
