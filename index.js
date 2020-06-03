b76fd0f73cfc93cd695f7cc56dce81b9f290b0b6
Jpendy <Jpendy256@gmail.com>
2020-06-03 15:29:53 -0700

    made list flag test

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const Note = require('./lib/Notes');
-const Input = require('./lib/Input');
+// const Input = require('./lib/Input');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:3000/note')
 
+mongoose.connect('mongodb://localhost:27017/notes', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use(express.json());
 
 // const input = new Input(process.argv);
 // Input.valid && Notes.execute(input);
 
+
+app.post('/notes', (req, res) => {
+  Note
+    .create(req.body)
+    .then(note => res.send(note));
+});
+
+app.get('/notes', (req, res) => {
+  Note
+    .find()
+    .then(note => res.send(note));
+});
 
 
 app.listen(3000, () => {
