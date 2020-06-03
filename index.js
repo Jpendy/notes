6a6cdbb5253b1e9115f972a01a343c0b360d7fce
Jpendy <Jpendy256@gmail.com>
2020-06-03 13:56:13 -0700

    mongo branch

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const { Notes } = require('./lib/Notes')
+const Note = require('./lib/Notes')
 const Input = require('./lib/Input')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:3000/note')
 
 
 const input = new Input(process.argv);
 Input.valid && Notes.execute(input);
+
+
+
+app.listen(3000, () => {
+    console.log('Started on 3000');
+});
